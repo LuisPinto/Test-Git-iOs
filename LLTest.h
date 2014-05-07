78d563af3020b9f674fb960951299380464f3382
Luis Pinto <luis.pinto@sodiumzine.com>
2014-05-07 22:07:53 +0200

    update

---
+++ LLTest.h
@@ -0,0 +1,13 @@
+//
+//  LLTest.h
+//  Gittest
+//
+//  Created by Luis Pinto on 07/05/14.
+//  Copyright (c) 2014 Luis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LLTest : NSObject
+
+@end
---
+++ LLTest.m
@@ -0,0 +1,13 @@
+//
+//  LLTest.m
+//  Gittest
+//
+//  Created by Luis Pinto on 07/05/14.
+//  Copyright (c) 2014 Luis. All rights reserved.
+//
+
+#import "LLTest.h"
+
+@implementation LLTest
+
+@end
